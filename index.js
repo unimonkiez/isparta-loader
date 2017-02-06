45ed4eeeae06601693075269129c64759010258a
Yuval Saraf <yuvalsaraf@gmail.com>
2017-02-06 10:06:46 +0200

    Look for query first on loader for getting config

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 'use strict';
 
 var isparta = require('isparta');
+var loaderUtils = require('loader-utils');
+
+var defaultConfig = {
+    embedSource: true,
+    noAutoWrap: true,
+    babel: undefined
+};
 
 module.exports = function(source) {
-    var config = this.options.isparta || {
-        embedSource: true,
-        noAutoWrap: true,
-        babel: this.options.babel
-    };
+    var config = this.query && Object.assign({}, defaultConfig, loaderUtils.parseQuery(this.query));
+
+    // If query not given fallback to default.
+    // Also try to take some configutation from `this.options`, which only works on webpack 1
+    if (!config) {
+        config = this.options.isparta || Object.assign({}, defaultConfig, {
+            babel: this.options.babel
+        });
+    }
 
     var instrumenter = new isparta.Instrumenter(config);
 
